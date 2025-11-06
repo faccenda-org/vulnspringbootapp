50edde486a7680752c3bbade77dffcd7eaf4bd91
Mauro Faccenda <faccenda@gmail.com>
2025-11-06 17:40:49 +0100

    initial test

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    id("java")
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    val javaVersion = providers.gradleProperty("javaVersion").get().toInt()
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(javaVersion))
+    }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+
+
+

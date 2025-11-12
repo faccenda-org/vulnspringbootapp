6fc534799cea2887e0edade6b57f45a9a2767a30
Mauro Faccenda <faccenda@gmail.com>
2025-11-12 14:23:00 +0000

    force java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,15 +7,19 @@ plugins {
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 
+java {
+    // Restore toolchain to ensure Gradle compiles with JDK 21 (class file major version 65)
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
 dependencies {
     val springBootVersion = providers.gradleProperty("springBootVersion").get()
     implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
     implementation("org.apache.commons:commons-text:1.9")
 }
 
-configurations.all {
-    resolutionStrategy.force("org.apache.commons:commons-text:1.9")
-}
 dependencyLocking {
     lockAllConfigurations()
 }
